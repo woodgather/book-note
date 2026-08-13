4978b9e5ce0c9071dd32ccabf543cf577e960765
fung <15875515020@163.com>
2026-08-13 16:28:45 +0800

    [config] update code 2026-08-13 16:28
    Signed-off-by: fung <15875515020@163.com>

--- upload_bk.bat
+++ upload_bk.bat
@@ -26,7 +26,7 @@ git pull
 git add -A
 
 :: 提交更改
-git commit -sm "[config] update home config %date_time%"
+git commit -sm "[config] update code %date_time%"
 
 :: 检查git commit是否成功
 if %errorlevel% equ 0 (
